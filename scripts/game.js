ca61a24ffe2f97608e3378f1881bc55e99e37479
Iris-Smok <irissmok11@gmail.com>
2022-02-06 09:47:25 +0000

    Create tests for basic game objects

---
+++ scripts/game.js
@@ -0,0 +1,8 @@
+let game = {
+  score: 0,
+  currentGame: [],
+  playerMoves: [],
+  choices: ["button1", "button2", "button3", "button4"],
+};
+
+module.exports = { game }; // we'll exporting more than one object and fuction from this file so we need to put them in curly braces
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,32 @@
 /**
  * @jest-environment jsdom
  */
+
+const { game } = require("../game");
+
+// function to load index.html file into the DOM
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContents = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
+});
+
+describe("game object contains correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices containes correct id's", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]); //we're picking out the choices key from our game object.
+  }); // we used Jest to equal matcher and we expect the array values od button1...
+});
