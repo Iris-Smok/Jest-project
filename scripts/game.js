e4693b70ed5b346e6f7dafcca94e55cb9bc9be05
Iris-Smok <irissmok11@gmail.com>
2022-02-07 08:25:00 +0000

    Add tests and functions for newGame,showScore, lightsOn, showTurns

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,46 @@ let game = {
   score: 0,
   currentGame: [],
   playerMoves: [],
+  turnNumber: 0,
   choices: ["button1", "button2", "button3", "button4"],
 };
 
-module.exports = { game }; // we'll exporting more than one object and fuction from this file so we need to put them in curly braces
+function newGame() {
+  game.score = 0;
+  game.playerMoves = [];
+  game.currentGame = [];
+  turnNumber = 0;
+  showScore();
+  addTurn();
+}
+
+function showScore() {
+  document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+  game.playerMoves = []; //clear the playerMoves
+  game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+  // we're going to push onto the computer game sequance, we're going to go to our game.choice key
+  showTurns(); // which contains our four values. The IDs of our buttons. Use math random library  to generate a random number between 0 and 3
+}
+
+function lightsOn(circ) {
+  document.getElementById(circ).classList.add("light");
+  setTimeout(() => {
+    document.getElementById(circ).classList.remove("light");
+  }, 400);
+}
+
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns }; // we'll exporting more than one object and fuction from this file so we need to put them in curly braces
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,15 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { TestWatcher } = require("jest");
+const {
+  game,
+  newGame,
+  showScore,
+  addTurn,
+  lightsOn,
+  showTurns,
+} = require("../game");
 
 // function to load index.html file into the DOM
 beforeAll(() => {
@@ -29,4 +37,65 @@ describe("game object contains correct keys", () => {
   test("choices containes correct id's", () => {
     expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]); //we're picking out the choices key from our game object.
   }); // we used Jest to equal matcher and we expect the array values od button1...
+  test("turn number key exists", () => {
+    expect("turnNumber" in game).toBe(true);
+  });
+});
+
+describe("newGame works correctly", () => {
+  beforeAll(() => {
+    // beforeAll runs before all of the test
+    //beforeAll function because we want to set the game state with some fake values to see if the newGame function resets them
+    game.score = 42;
+    game.playerMoves = ["button1", "button2"];
+    game.currentGame = ["button1", "button2"];
+    game.turnNumber = 2;
+    document.getElementById("score").innertext = "42";
+    newGame();
+  });
+  test("should set game score to zero", () => {
+    expect(game.score).toEqual(0);
+  });
+  test("should be one move in the computer's game array", () => {
+    expect(game.currentGame.length).toBe(1);
+  });
+
+  test("should clear the currentGame array", () => {
+    expect(game.currentGame.length).toBe(1); // you can use toEqual as well
+  });
+  test("should set turnNumber to zero", () => {
+    expect(game.turnNumber).toBe(0);
+  });
+  test("should display 0 for the element with an id of score", () => {
+    expect(document.getElementById("score").innerText).toEqual(0);
+  });
+});
+
+describe("gameplay works correctly", () => {
+  beforeEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    addTurn();
+  });
+  afterEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+  });
+  test("addTurn adds a new turn to the game", () => {
+    addTurn();
+    expect(game.currentGame.length).toBe(2);
+  });
+  test("should add correct class to Light up the buttons", () => {
+    // to check is correct class has been added to our button to light ip up,we'll go for the first element in the currentGame array because we know there'll always be at least one element there
+    let button = document.getElementById(game.currentGame[0]); // get one of the IDs from currentGame array and assign it to a varible
+    lightsOn(game.currentGame[0]); // and call lightOn function
+    expect(button.classList).toContain("light");
+  });
+  test("showTurns should update game.turnNumber", () => {
+    game.turnNumber = 42;
+    showTurns();
+    expect(game.turnNumber).toBe(0);
+  });
 });
